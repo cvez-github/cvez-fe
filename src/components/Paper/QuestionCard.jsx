d248c6a7f26ee543f277b17afca6ae85cb9d688f
DiepDong <diepgiadong@gmail.com>
2024-03-29 09:40:03 +0700

    sprint2/feat: add

--- src/components/Paper/QuestionCard.jsx
+++ src/components/Paper/QuestionCard.jsx
@@ -1,4 +1,4 @@
-import { Paper, Flex, Grid } from '@mantine/core';
+import { Fieldset, Flex, Grid } from '@mantine/core';
 import Area from '../Input/Textarea';
 import Tinput from '../Input/Input';
 import Addselect from '../Select/Select';
@@ -8,19 +8,19 @@ import AddButton from '../Button/Button';
 export default function QuesCard({ QuesCont, Answer, Levels }) {
     return (
         <>
-            <Paper shadow="lg" withBorder p="xl"> 
+            <Fieldset w="100%" variant='unstyled'>
                 <Flex direction="column" gap="md">
-                <Area title={QuesCont}  />
-                <Grid grow>
-                <Grid.Col span={9}><Tinput title={Answer}/></Grid.Col>
-                <Grid.Col span={1}> <Addselect title={Levels}/> </Grid.Col>
-                </Grid>
-                <Flex justify="flex-end" gap="md">
-                <AddButton v={"subtle"} c={"gray"} title={"Cancel"}/>
-                <AddButton v={"Filled"}  title={"Ok"}/>
+                    <Area title={QuesCont} />
+                    <Grid grow>
+                        <Grid.Col span={9}><Tinput title={Answer} /></Grid.Col>
+                        <Grid.Col span={1}> <Addselect title={Levels} /> </Grid.Col>
+                    </Grid>
+                    <Flex justify="flex-end" gap="md">
+                        <AddButton v={"subtle"} c={"gray"} title={"Cancel"} />
+                        <AddButton v={"Filled"} title={"Ok"} />
+                    </Flex>
                 </Flex>
-                </Flex>
-            </Paper>
+            </Fieldset>
         </>
     );
 }
